5ea4a2bd1b03532e0a35c50ac62085173b0c156a
DDCcarr1 <ddccarr1@icloud.com>
2026-02-09 20:49:06 +0000

    finish datetypes

---
+++ w3schools/datatypes/auto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    auto num = 10;//sets num as an integer because 10 is an integer
+    auto dec = 5.5;//sets dec as a double because 5.5 is a double
+    auto letter = 'A';//sets letter as a char because 'A is a character
+
+    cout << "Integer: " << num << endl;
+    cout << "Double: " << dec << endl;
+    cout << "Character: " << letter << endl;
+
+    return 0;
+}
---
+++ w3schools/datatypes/challenge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int studentID = 1022836;
+  float score = 69.67;
+  char grade = 'F';
+
+  cout << studentID << score << grade << endl;
+
+  return 0;
+}
