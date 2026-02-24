1f80cd4d4c3ee4fd9170858f270f0676b58f751d
DDCcarr1 <ddccarr1@icloud.com>
2026-02-24 20:03:58 +0000

    finally up to my progress

---
+++ w3schools/booleans/values.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << isCodingFun << "\n";   // Outputs true
+    cout << boolalpha; // enable printing "true"/"false"
+    cout << isFishTasty << "\n";  // Outputs false
+
+    return 0;
+}
---
+++ w3schools/math/math.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << max(5, 10) << endl;//prints the maximum of 5 and 10, then adds a newline
+    cout << min(5, 10) << endl;//prints the minimum of 5
+    cout << sqrt(64) << endl;//prints the square root of 64, then adds a newline
+    cout << round(2.6) << endl;//prints 3, then adds a
+    cout << log(2) << endl;//prints the natural logarithm of 2, then adds a newline
+
+    return 0;
+}
