97075f07a72ea80a3ce9eabbb30d63e8ce3d8cbe
DDCcarr1 <ddccarr1@icloud.com>
2026-02-27 19:07:08 +0000

    fvcvxzcvx

---
+++ w3schools/while/while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    while (i < 5) {//while loop
+        cout << i << endl;
+        i++;//increment i by 1
+    }//output: 0 1 2 3 4
+
+    return 0;
+}
