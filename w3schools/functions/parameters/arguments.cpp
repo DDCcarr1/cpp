f71157b8ea18b461ccf566fc0f6073339426324d
DDCcarr1 <ddccarr1@icloud.com>
2026-04-24 19:32:51 +0000

    ay

---
+++ w3schools/functions/parameters/arguments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(int x, int y) {
+  cout << "The value of x is: " << x << endl;
+  cout << "The value of y is: " << y << endl;
+}
+
+int main() {
+  myFunction(5, 10);
+
+  return 0;
+}
